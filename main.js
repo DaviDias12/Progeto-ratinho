3d076a161b97c9b1976c4739589459f06ae9ef68
Davi Santos <davirr1220@gmail.com>
2022-10-30 21:05:58 -0300

    implementando o js

---
+++ main.js
@@ -0,0 +1,12 @@
+  áudios  =  [
+    { caminho : 'audios/grito.mp3' ,  legenda : 'GRITO' } ,
+    { caminho : 'audios/jesus.mp3' ,  legenda : 'Jesus' } ,
+    { caminho : 'audios/nao-eo-pai.mp3' ,  legenda : 'Ele não é o pai' } ,
+    { caminho : 'audios/pare.mp3' ,  legenda : 'PARE' } ,
+    { caminho : 'audios/que-isso-meu-filho.mp3' ,  legenda : 'Que isso meu fio' } ,
+    { caminho : 'audios/ratinhoo.mp3' ,  legenda : 'RATINHO' } ,
+    { caminho : 'audios/uepa.mp3' ,  legenda : 'UEPA' } ,
+    { caminho : 'audios/vixi-ratinho.mp3' ,  legenda : 'VIXI' } ,
+    { caminho : 'audios/xaropinho.mp3' ,  legenda : 'Xaropinho' } ,
+] ;
+
