01f6b4390b3a46ce9ca7a985fca37d3ebcc8f7b9
Davi Santos <davirr1220@gmail.com>
2022-11-08 15:42:48 -0300

    finalizando projeto

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 let audios = [
-  {caminho:'audios/grito.mp3', legenda:'GRITO'},
-  {caminho:'audios/jesus.mp3', legenda:'Jesus'},
-  {caminho:'audios/nao-e-o-pai.mp3', legenda:'Ele não é o pai'},
-  {caminho:'audios/pare.mp3', legenda:'PARE'},
-  {caminho:'audios/que-isso-meu-filho.mp3', legenda:'Que isso meu fio'},
-  {caminho:'audios/ratinhoo.mp3', legenda:'RATINHO'},
-  {caminho:'audios/uepa.mp3', legenda:'UEPA'},
-  {caminho:'audios/vixi-ratinho.mp3', legenda:'VIXI'},
-  {caminho:'audios/xaropinho.mp3', legenda:'Xaropinho'},
+  {caminho:'Audio/projeto_ratinho_audios_grito.mp3', legenda:'GRITO'},
+  {caminho:'Audio/projeto_ratinho_audios_jesus.mp3', legenda:'Jesus'},
+  {caminho:'Audio/projeto_ratinho_audios_nao-e-o-pai.mp3', legenda:'Ele não é o pai'},
+  {caminho:'Audio/projeto_ratinho_audios_pare.mp3', legenda:'PARE'},
+  {caminho:'Audio/projeto_ratinho_audios_que-isso-meu-filho.mp3', legenda:'Que isso meu fio'},
+  {caminho:'Audio/projeto_ratinho_audios_ratinhoo.mp3', legenda:'RATINHO'},
+  {caminho:'Audio/projeto_ratinho_audios_uepa.mp3', legenda:'UEPA'},
+  {caminho:'Audio/projeto_ratinho_audios_vixi-ratinho.mp3', legenda:'VIXI'},
+  {caminho:'Audio/projeto_ratinho_audios_xaropinho.mp3', legenda:'Xaropinho'},
 ];
 
 let botoes = document.querySelectorAll('.botao');
 let legendas = document.querySelectorAll('p');
-
 
 for (let i=0; i < 9; i++){
   legendas[i].textContent = audios[i].legenda;
